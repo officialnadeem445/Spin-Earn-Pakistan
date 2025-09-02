613bf0e959c84c8660ef73c6932c7b025dfea7e7
officialnadeem445 <109586075+officialnadeem445@users.noreply.github.com>
2025-09-02 17:05:08 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,16 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    outDir: "dist", // ✅ Vercel ke liye safe
+    emptyOutDir: true,
   },
 })
